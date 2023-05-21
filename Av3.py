1e948d402d2494bfacf23b922139401c13707295
GabsFMA <GabrielfmAmaral@gmail.com>
2023-05-21 16:51:17 -0300

    commit 2

--- Av3.py
+++ Av3.py
@@ -1,8 +1,8 @@
 print('Insira a seguir suas notas da Av1, Av2 e do Edag!\n')
 
-av1 = float(input('Nota da AV1: '))
-av2 = float(input('Nota da AV2: '))
-edag = float(input('Nota do EDAG: '))
+av1 = float(input('Notas da Av1: '))
+av2 = float(input('Notas da Av2: '))
+edag = float(input('Notas do EDAG: '))
 
 av3 = ( 7 - ((av1*0.25) + (av2*0.25) + (edag*0.2)) ) / 0.3
 
