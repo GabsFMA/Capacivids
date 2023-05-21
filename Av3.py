154e659a585ee38e48eae8b5e95c8ed220633ccb
GabsFMA <GabrielfmAmaral@gmail.com>
2023-05-21 16:26:47 -0300

    v. com quebra de linha

--- Av3.py
+++ Av3.py
@@ -1,7 +1,9 @@
+print('Insira a seguir suas notas da Av1, Av2 e do Edag!\n')
+
 av1 = float(input('Nota da AV1: '))
 av2 = float(input('Nota da AV2: '))
 edag = float(input('Nota do EDAG: '))
 
 av3 = ( 7 - ((av1*0.25) + (av2*0.25) + (edag*0.2)) ) / 0.3
 
-print('Precisará de {:.2f} na av3'.format(av3))
+print('\nPrecisará de {:.2f} na av3'.format(av3))
